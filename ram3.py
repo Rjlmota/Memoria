91ecb49e117fa54c3e25c00c46f7bab4a9d012e9
Alberto <aclsobrinho@hotmail.com>
2018-10-08 19:15:44 -0300

    Classes em python 3.

--- ram3.py
+++ ram3.py
@@ -1,16 +1,19 @@
-def write():
-    adress = input("Indique um endereço de 4 bits: ")
-    value = input("Indique o valor de 8 bits: ")
+class memoryram:
 
-    slots[adress.zfill(4)] = value.zfill(8)
+	def __init__(self):
+		self.slots = {}
+		for i in range(0, 16):
+	    		self.slots[bin(i)[2:].zfill(4)] = "00000000"
+
+	def write(self):
+	    adress = input("Indique um endereço de 4 bits: ")
+	    value = input("Indique o valor de 8 bits: ")
+
+	    self.slots[adress.zfill(4)] = value.zfill(8)
 
 
-def read():
-    adress = input("Indique um endereco de 4 bits: ")
-    print(slots[adress.zfill(4)])
+	def read(self):
+	    adress = input("Indique um endereco de 4 bits: ")
+	    print(self.slots[adress.zfill(4)])
 
 
-slots = {}
-
-for i in range(0, 16):
-    slots[bin(i)[2:].zfill(4)] = "00000000"
