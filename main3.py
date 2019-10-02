490c08dae027335b67415e255e1384b22cda801f
hwmanz <52043847+hwmanz@users.noreply.github.com>
2019-10-02 12:54:21 -0300

    Update main3.py

--- main3.py
+++ main3.py
@@ -4,13 +4,13 @@ op = "R"
 
 while op in ("RWLrwl"):
 
-    op = input("Digite W para escrever, R para ler, L para listar toda a memoria  e qualquer tecla para parar. \n")
+    op = upper(input("Digite W para escrever, R para ler, L para listar toda a memoria e qualquer tecla para parar. \n"))
 
-    if op == "R" or op == "r":
+    if op == "R":
         ram3.read()
-    if op == "W" or op =="w":
+    if op == "W":
         ram3.write()
-    if op == "L" or op == "l":
+    if op == "L:
         for i in sorted(ram3.slots):
             print(i, ram3.slots[i])
 
